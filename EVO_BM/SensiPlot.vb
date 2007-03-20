b71fb3d918f5b5dfb6b50e6d641b552072eb37a0
huebner <c.huebner@bluemodel.org>
2007-03-20 12:37:47 +0000

    Branch FloodOpt Revision 103 bis Revision 165 in Trunk gemerged.

--- EVO_BM/SensiPlot.vb
+++ EVO_BM/SensiPlot.vb
@@ -38,4 +38,51 @@ Public Class SensiPlot
         Me.DialogResult = System.Windows.Forms.DialogResult.Cancel
         Me.Close()
     End Sub
+
+    '************************ TeeChart Funktionen ***************************************
+
+    Public Sub TeeChart_Ini_SensiPlot(ByRef TChart1 As Steema.TeeChart.TChart, ByVal NPopul As Short, ByVal BottomTitle As String, ByVal LeftTitle As String)
+        Dim Populationen As Short
+
+        Populationen = NPopul
+
+        With TChart1
+            .Clear()
+            .Header.Text = "BlauesModell"
+            .Aspect.View3D = False
+            .Legend.Visible = False
+
+            'Formatierung der Axen
+            .Chart.Axes.Bottom.Title.Caption = BottomTitle 'HACK: Beschriftung der Axen
+            .Chart.Axes.Bottom.Automatic = True
+            .Chart.Axes.Left.Title.Caption = LeftTitle 'HACK: Beschriftung der Axen
+            .Chart.Axes.Left.Automatic = True
+
+            'Series(0): Series für die Population.
+            Dim Point1 As New Steema.TeeChart.Styles.Points(.Chart)
+            Point1.Title = "Population"
+            Point1.Pointer.Style = Steema.TeeChart.Styles.PointerStyles.Circle
+            Point1.Color = System.Drawing.Color.Orange
+            Point1.Pointer.HorizSize = 2
+            Point1.Pointer.VertSize = 2
+
+            'Series(1): Series für die Sekundäre Population
+            Dim Point2 As New Steema.TeeChart.Styles.Points(.Chart)
+            Point2.Title = "Sekundäre Population"
+            Point2.Pointer.Style = Steema.TeeChart.Styles.PointerStyles.Circle
+            Point2.Color = System.Drawing.Color.Blue
+            Point2.Pointer.HorizSize = 3
+            Point2.Pointer.VertSize = 3
+
+            'Series(2): Series für Bestwert
+            Dim Point3 As New Steema.TeeChart.Styles.Points(.Chart)
+            Point3.Title = "Bestwerte"
+            Point3.Pointer.Style = Steema.TeeChart.Styles.PointerStyles.Circle
+            Point3.Color = System.Drawing.Color.Green
+            Point3.Pointer.HorizSize = 3
+            Point3.Pointer.VertSize = 3
+
+        End With
+    End Sub
+
 End Class
