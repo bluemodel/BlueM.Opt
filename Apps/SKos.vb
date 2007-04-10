8ef9c420ea5fc47d5a077ef3c473ccec4d71b966
huebner <c.huebner@bluemodel.org>
2007-04-10 06:53:36 +0000

    Einige Kleinigkeiten. Mit der Klasse für die Kosten begonnen.

---
+++ Apps/SKos.vb
@@ -0,0 +1,31 @@
+Public Class SKos
+
+
+    '*******************************************************************************
+    '*******************************************************************************
+    '**** Klasse SKos zur Ermittlung der Schäden und Kosten im HW Fall          ****
+    '****                                                                       ****
+    '**** Christoph Hübner                                                      ****
+    '****                                                                       ****
+    '**** Fachgebiet Ingenieurhydrologie und Wasserbewirtschaftung              ****
+    '**** TU Darmstadt                                                          ****
+    '****                                                                       ****
+    '**** Februar 2007                                                          ****
+    '****                                                                       ****
+    '**** Letzte Änderung: April 2007                                           ****
+    '*******************************************************************************
+    '*******************************************************************************
+
+    Public Function calculate_costs()
+        Dim costs As Double
+
+
+
+
+        Return costs
+    End Function
+
+    Private Sub create_Bauwerksliste()
+
+    End Sub
+End Class
