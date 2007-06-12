cf478a4a93e5e1953392203eca5ede9fbd2cf869
froehlich <froehlich@719ab8b7-b586-4fd8-a5da-7ca64381027e>
2007-06-12 13:37:14 +0000

    Tooltips für Diagramm-Buttons

--- trunk/_Main/DiagrammForm.Designer.vb
+++ trunk/_Main/DiagrammForm.Designer.vb
@@ -19,12 +19,14 @@ Partial Class DiagrammForm
     'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
         Me.GroupBox_TChartButtons = New System.Windows.Forms.GroupBox
         Me.Button_TChartEdit = New System.Windows.Forms.Button
         Me.Button_TChart2PNG = New System.Windows.Forms.Button
         Me.Button_TChartSave = New System.Windows.Forms.Button
         Me.Button_TChart2Excel = New System.Windows.Forms.Button
         Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
         Me.Diag = New Main.Diagramm
         Me.GroupBox_TChartButtons.SuspendLayout()
         Me.SuspendLayout()
@@ -49,6 +51,7 @@ Partial Class DiagrammForm
         Me.Button_TChartEdit.Name = "Button_TChartEdit"
         Me.Button_TChartEdit.Size = New System.Drawing.Size(25, 25)
         Me.Button_TChartEdit.TabIndex = 0
+        Me.ToolTip1.SetToolTip(Me.Button_TChartEdit, "Diagramm bearbeiten")
         Me.Button_TChartEdit.UseVisualStyleBackColor = True
         '
         'Button_TChart2PNG
@@ -58,6 +61,7 @@ Partial Class DiagrammForm
         Me.Button_TChart2PNG.Name = "Button_TChart2PNG"
         Me.Button_TChart2PNG.Size = New System.Drawing.Size(25, 25)
         Me.Button_TChart2PNG.TabIndex = 3
+        Me.ToolTip1.SetToolTip(Me.Button_TChart2PNG, "als PNG exportieren")
         Me.Button_TChart2PNG.UseVisualStyleBackColor = True
         '
         'Button_TChartSave
@@ -67,6 +71,7 @@ Partial Class DiagrammForm
         Me.Button_TChartSave.Name = "Button_TChartSave"
         Me.Button_TChartSave.Size = New System.Drawing.Size(25, 25)
         Me.Button_TChartSave.TabIndex = 2
+        Me.ToolTip1.SetToolTip(Me.Button_TChartSave, "als natives TeeChart-Format exportieren")
         Me.Button_TChartSave.UseVisualStyleBackColor = True
         '
         'Button_TChart2Excel
@@ -77,6 +82,7 @@ Partial Class DiagrammForm
         Me.Button_TChart2Excel.Name = "Button_TChart2Excel"
         Me.Button_TChart2Excel.Size = New System.Drawing.Size(25, 25)
         Me.Button_TChart2Excel.TabIndex = 1
+        Me.ToolTip1.SetToolTip(Me.Button_TChart2Excel, "nach Excel exportieren")
         Me.Button_TChart2Excel.UseVisualStyleBackColor = False
         '
         'SaveFileDialog1
@@ -214,5 +220,6 @@ Partial Class DiagrammForm
     Friend WithEvents Button_TChartSave As System.Windows.Forms.Button
     Friend WithEvents Button_TChart2Excel As System.Windows.Forms.Button
     Friend WithEvents SaveFileDialog1 As System.Windows.Forms.SaveFileDialog
+    Friend WithEvents ToolTip1 As System.Windows.Forms.ToolTip
 
 End Class
