3d6f3f520c0c465a210f988e91eabe44892c241b
froehlich <froehlich@719ab8b7-b586-4fd8-a5da-7ca64381027e>
2007-06-12 14:16:59 +0000

    Scatterplot-Matrix eingebaut Bug 118

--- trunk/_Main/My Project/Resources.Designer.vb
+++ trunk/_Main/My Project/Resources.Designer.vb
@@ -87,5 +87,12 @@ Namespace My.Resources
                 Return CType(obj,System.Drawing.Bitmap)
             End Get
         End Property
+        
+        Friend ReadOnly Property scatterplot() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("scatterplot", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
     End Module
 End Namespace
