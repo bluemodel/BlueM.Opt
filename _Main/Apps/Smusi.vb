f0a53e8a6a337a8bb811ef17bab2be9e52818301
froehlich <f.froehlich@bluemodel.org>
2007-08-13 15:09:29 +0000

    Constraints fuer PES ermoeglicht (CES nur ansatzweise). TODO: * Constraints fuer CES * Bei Constraint-Verletzung Punkte in TeeChart anders darstellen * Constraintberechnung fuer Reihen verbessern Bug 112

--- _Main/Apps/Smusi.vb
+++ _Main/Apps/Smusi.vb
@@ -157,7 +157,7 @@ Public Class Smusi
 
     'Schreibt die neuen Verzweigungen
     '********************************
-    Protected Overrides Sub Prepere_Write_Verzweigungen()
+    Protected Overrides Sub Prepare_Write_Verzweigungen()
 
     End Sub
 
