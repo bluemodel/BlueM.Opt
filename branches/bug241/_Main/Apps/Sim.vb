c9fae05c42b75c8df54e672d86f2602eaddde01a
froehlich <froehlich@719ab8b7-b586-4fd8-a5da-7ca64381027e>
2007-11-20 13:04:27 +0000

    Merged revisions 606-613 via svnmerge from http://130.83.196.154/svn/EVO.NET/trunk
    ........
      r607 | froehlich | 2007-11-16 15:27:01 +0100 (Fr, 16 Nov 2007) | 1 line
      
      Klasse OptResult in separate Datei ausgelagert
    ........

--- branches/bug241/_Main/Apps/Sim.vb
+++ branches/bug241/_Main/Apps/Sim.vb
@@ -2237,36 +2237,3 @@ Public MustInherit Class Sim
 #End Region 'Methoden
 
 End Class
-
-'zusätzliche Klassen
-'###################
-
-'Klasse OptResult
-'enthält die Ergebnisse eines Optimierungslaufs
-'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-Public Class OptResult
-
-    'Optimierungsbedingungen
-    Public List_OptZiele() As Sim.Struct_OptZiel
-    Public List_OptParameter() As Sim.Struct_OptParameter
-    Public List_Constraints() As Sim.Struct_Constraint
-
-    'Structure einer Lösung
-    Public Structure Struct_Solution
-        Public QWerte() As Double
-        Public OptPara() As Double
-        Public Constraints() As Double
-        Public ReadOnly Property isValid() As Boolean
-            Get
-                For i As Integer = 0 To Me.Constraints.GetUpperBound(0)
-                    If (Me.Constraints(i) < 0) Then Return False
-                Next
-                Return True
-            End Get
-        End Property
-    End Structure
-
-    'Array von Lösungen
-    Public Solutions() As Struct_Solution
-
-End Class
