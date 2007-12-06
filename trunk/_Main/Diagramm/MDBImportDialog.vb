8853ef784b2080ef10382dbc19eda30168204db6
muschalla <muschalla@719ab8b7-b586-4fd8-a5da-7ca64381027e>
2007-12-06 14:07:15 +0000

    * Datenbank Anbindung auch für Single-objective * SMUSI-Anpassungen

--- trunk/_Main/Diagramm/MDBImportDialog.vb
+++ trunk/_Main/Diagramm/MDBImportDialog.vb
@@ -3,32 +3,36 @@ Public Class MDBImportDialog
     'Form load
     '*********
     Private Sub MDBImportDialog_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-
-        Me.ComboBox_SekPop.SelectedIndex = 0
-
+        If Form1.Sim1.List_OptZiele.Length < 2 Then
+            Me.ComboBox_SekPop.SelectedIndex = 1
+        Else
+            Me.ComboBox_SekPop.SelectedIndex = 0
+        End If
     End Sub
 
     'Überprüfung der Benutzereingabe
     '*******************************
     Private Sub OK_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OK_Button.Click
-
-        'Mindestens eine X- und Y-Achse ausgewählt?
-        If (Me.ListBox_OptZieleX.SelectedIndex = -1 Or Me.ListBox_OptZieleY.SelectedIndex = -1) Then
-            MsgBox("Bitte mindestens eine X- und eine Y-Achse auswählen!", MsgBoxStyle.Exclamation, "Fehler")
-            Me.DialogResult = Windows.Forms.DialogResult.None
-            Exit Sub
+        If (Me.ListBox_OptZieleY.Enabled) Then
+            'Mindestens eine X- und Y-Achse ausgewählt?
+            If (Me.ListBox_OptZieleX.SelectedIndex = -1 Or Me.ListBox_OptZieleY.SelectedIndex = -1) Then
+                MsgBox("Bitte mindestens eine X- und eine Y-Achse auswählen!", MsgBoxStyle.Exclamation, "Fehler")
+                Me.DialogResult = Windows.Forms.DialogResult.None
+                Exit Sub
+            End If
         End If
 
         'Unterschiedliche Achsen ausgewählt?
-        If (Me.ListBox_OptZieleX.SelectedIndex = Me.ListBox_OptZieleY.SelectedIndex Or _
-            (Not Me.ListBox_OptZieleZ.SelectedIndex = -1 And _
-                (Me.ListBox_OptZieleX.SelectedIndex = Me.ListBox_OptZieleZ.SelectedIndex Or _
-                Me.ListBox_OptZieleY.SelectedIndex = Me.ListBox_OptZieleZ.SelectedIndex))) Then
-            MsgBox("Achsen müssen unterschiedlich sein!", MsgBoxStyle.Exclamation, "Fehler")
-            Me.DialogResult = Windows.Forms.DialogResult.None
-            Exit Sub
+        If (Me.ListBox_OptZieleY.Enabled) Then
+            If (Me.ListBox_OptZieleX.SelectedIndex = Me.ListBox_OptZieleY.SelectedIndex Or _
+                (Not Me.ListBox_OptZieleZ.SelectedIndex = -1 And _
+                    (Me.ListBox_OptZieleX.SelectedIndex = Me.ListBox_OptZieleZ.SelectedIndex Or _
+                    Me.ListBox_OptZieleY.SelectedIndex = Me.ListBox_OptZieleZ.SelectedIndex))) Then
+                MsgBox("Achsen müssen unterschiedlich sein!", MsgBoxStyle.Exclamation, "Fehler")
+                Me.DialogResult = Windows.Forms.DialogResult.None
+                Exit Sub
+            End If
         End If
-
     End Sub
 
 End Class
