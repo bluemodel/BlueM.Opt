24902efe825a46089c3978ce8b1e5a473e2ec240
froehlich <froehlich@719ab8b7-b586-4fd8-a5da-7ca64381027e>
2007-12-10 16:25:46 +0000

    SMUSI großgeschrieben

--- trunk/_Main/EVOMOD.vb
+++ trunk/_Main/EVOMOD.vb
@@ -2,7 +2,7 @@ Module EVOMOD
 
     'Anwendungen
     Public Const ANW_BLUEM As String = "BlueM"
-    Public Const ANW_SMUSI As String = "Smusi"
+    Public Const ANW_SMUSI As String = "SMUSI"
     Public Const ANW_SCAN As String = "S:CAN"
     Public Const ANW_SWMM As String = "SWMM"
     Public Const ANW_TESTPROBLEME As String = "Testprobleme"
