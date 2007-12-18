c47a307b59cccb325a3aaebaafbd9f1af434bd17
huebner <c.huebner@bluemodel.org>
2007-12-18 13:18:03 +0000

    Struct für die CES Settings eingeführt.

--- EVO.Kern/EVO_Settings.vb
+++ EVO.Kern/EVO_Settings.vb
@@ -12,8 +12,8 @@ Public Class EVO_Settings
     '*******************************************************************************
     '*******************************************************************************
 
-    'Struktur PES_Settings
-    '---------------------
+    'Struct für die PES_Settings
+    '---------------------------
     Public Structure PES_Settings
 
         Public ty_EvoModus As EVO_MODUS            'Single- oder Multi-Objective
@@ -114,6 +114,86 @@ Public Class EVO_Settings
         End Sub
 
     End Structure
+
+    'Struct für die CES_Settings
+    '---------------------------
+    Public Structure CES_Settings
+
+        'CES
+        Public n_Parts_of_Path As Integer           'Länge des Gedächtnispfades Achtung Maximum ist 3
+        Public n_Generations As Integer             'Anzahl der Generationen
+        Public n_Parents As Integer
+        Public n_Childs As Integer
+        Public n_MemberSecondPop As Integer
+        Public n_Interact As Integer
+        Public is_Interact As Boolean
+        Public ReprodOperator As String
+        Public MutOperator As String
+        Public Strategy As String                   '"plus" oder "minus" Strategie
+        Public MutRate As Integer                   'Definiert die Wahrscheinlichkeit der Mutationsrate in %
+
+        'Hybrid
+        Public n_PES_Parents As Integer             'Anzahl der Eltern für PES Hybrid
+        Public n_PES_MemberSecondPop As Integer
+        Public n_PES_Interact As Integer
+        Public is_PES_Interact As Boolean
+        Public is_Pop As Boolean                    'Gibt an ob bei der Population oder bei den Eltern in die PES gestiegen wird.
+
+        'Standardwerte setzen
+        '********************
+        Public Sub setStandard(ByVal modus As Kern.EVO_MODUS)
+
+            Select Case modus
+
+                Case EVO_MODUS.Single_Objective
+
+                    'CES
+                    Me.n_Parts_of_Path = 3
+                    Me.n_Generations = 500
+                    Me.n_Parents = 5
+                    Me.n_Childs = 15
+                    Me.n_MemberSecondPop = 50
+                    Me.n_Interact = 5
+                    Me.is_Interact = True
+                    Me.ReprodOperator = "Select_Random_Uniform"
+                    Me.MutOperator = "RND_Switch"
+                    Me.Strategy = "plus"
+                    Me.MutRate = 25
+
+                    'Hybrid
+                    Me.n_PES_Parents = 5
+                    Me.n_PES_MemberSecondPop = 50
+                    Me.n_PES_Interact = 3
+                    Me.is_PES_Interact = False
+                    Me.is_Pop = False
+
+                Case EVO_MODUS.Multi_Objective
+
+                    'CES
+                    Me.n_Parts_of_Path = 3
+                    Me.n_Generations = 500
+                    Me.n_Parents = 5
+                    Me.n_Childs = 15
+                    Me.n_MemberSecondPop = 50
+                    Me.n_Interact = 5
+                    Me.is_Interact = True
+                    Me.ReprodOperator = "Select_Random_Uniform"
+                    Me.MutOperator = "RND_Switch"
+                    Me.Strategy = "plus"
+                    Me.MutRate = 25
+
+                    'Hybrid
+                    Me.n_PES_Parents = 5
+                    Me.n_PES_MemberSecondPop = 50
+                    Me.n_PES_Interact = 3
+                    Me.is_PES_Interact = False
+                    Me.is_Pop = False
+
+            End Select
+        End Sub
+
+    End Structure
+
     Public Structure HookJeeves_Settings
         Public DnStart As Double                   'Startschrittweite
         Public is_DnVektor As Boolean              'Soll ein Schrittweitenvektor benutzt werden
