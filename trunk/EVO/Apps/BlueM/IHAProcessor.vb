07600b5d09f3e3c6a02154cff1959e9bbece35aa
froehlich <froehlich@719ab8b7-b586-4fd8-a5da-7ca64381027e>
2008-02-16 19:36:40 +0000

    Zielfunktion IHA: bei RVA-Vergleich ist QWert = (1 - diff) ^ 2 Bug 267

--- trunk/EVO/Apps/BlueM/IHAProcessor.vb
+++ trunk/EVO/Apps/BlueM/IHAProcessor.vb
@@ -105,7 +105,7 @@ Public Class IHAProcessor
                     Next
                 End If
 
-                QWert = 1 - diff
+                QWert = (1 - diff) ^ 2
 
         End Select
 
@@ -133,10 +133,14 @@ Public Class IHAProcessor
             For j = 0 To RVAResult.IHAParamGroups(i).NParams - 1
                 PGroup_Sum_fx(i) += fx(RVAResult.IHAParamGroups(i).IHAParams(j).HAMiddle)
             Next
+
+            'Mittelwert für eine Parametergruppe
             RVAfx.PGroup_Avg_fx(i) = PGroup_Sum_fx(i) / RVAResult.IHAParamGroups(i).NParams
 
             All_Sum_fx += RVAfx.PGroup_Avg_fx(i)
         Next
+
+        'Mittelwert über alle Parametergruppen
         RVAfx.All_Avg_fx = All_Sum_fx / RVAResult.NGroups
 
         Return RVAfx
@@ -147,7 +151,7 @@ Public Class IHAProcessor
     '***************************************************************************************
     Private Shared Function fx(ByVal HA As Double) As Double
 
-        'Parameter für Normalverteilung mit f(0) ~= 1, f(-1) ~= 0
+        'Parameter für Normalverteilung mit fx(0) ~= 1 und fx(-1) = fx(1) ~= 0
         '[EXCEL:] 1/(std*WURZEL(2*PI()))*EXP(-1/2*((X-avg)/std)^2)
         Dim std As Double = 0.398942423706863                   'Standardabweichung
         Dim avg As Double = 0                                   'Erwartungswert
