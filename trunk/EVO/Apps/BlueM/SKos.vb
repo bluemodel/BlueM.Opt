4fc685871ab101d9667518eb9b7cf7e663fedbb3
huebner <huebner@719ab8b7-b586-4fd8-a5da-7ca64381027e>
2008-07-03 16:29:48 +0000

    Auslesen der Länge durch neue Version ersetzt.

--- trunk/EVO/Apps/BlueM/SKos.vb
+++ trunk/EVO/Apps/BlueM/SKos.vb
@@ -21,28 +21,38 @@ Public Class SKos
     Public Akt_Elemente() As String
     Public Save_TRS(,) as String
 
+    'Struktur welche Informationen der Transportstrecken entält
+    '**********************************************************
     Public Structure TRS
 
         Dim Name as String
-        Dim Laenge as Double
+        Dim Laenge As Double
+        Dim Volume As Double
+        Dim Costs As Double
 
-        Dim A_Breite_Sohle as Double
+        Dim A0_Hoehe_Sohle As Double
+        Dim A1_Breite_Sohle_L As Double
+        Dim A2_Breite_Sohle_R As Double
 
-        Dim B1_Höhe_Gerinne as Double
-        Dim B2_Breite_Ufer as Double
+        Dim B0_Hoehe_Gerinne As Double
+        Dim B1_Breite_Ufer_L As Double
+        Dim B2_Breite_Ufer_R As Double
 
-        Dim C1_Höhe_FPlain as Double
-        Dim C2_Breite_Vorl as Double
+        Dim C0_Hoehe_FPlain As Double
+        Dim C1_Breite_Vorl_L As Double
+        Dim C2_Breite_Vorl_R As Double
 
-        Dim D1_Höhe_Rand as Double
-        Dim D2_Breite_Rand as Double
+        Dim D0_Hoehe_Rand As Double
+        Dim D1_Breite_Rand_L As Double
+        Dim D2_Breite_Rand_R As Double
+
 
     End Structure
 
-    Dim TRS_Orig(-1) as TRS
-    Dim TRS_Akt(-1) as TRS
+    Dim TRS_Orig(-1) As TRS
+    Dim TRS_Akt(-1) As TRS
 
-    
+
     'Funktion für die Kalkulation der Kosten
     '***************************************
     Public Function Calculate_Costs(ByVal BlueM1 As BlueM) As Double
@@ -55,11 +65,11 @@ Public Class SKos
         Call create_Elementliste(BlueM1, Elementliste)
 
         'Ermitteln der massgeblichen Größen
-        Call Read_TRS_Laenge(BlueM1, TRS_Array)
         Call Read_TAL(BlueM1, TAL_Array)
-        
+        Call Read_TRS_Daten(BlueM1, TRS_Akt)
+
         'Kalkulieren der Kosten für jedes Bauwerk
-        Call Acquire_Costs(TRS_Array, TAL_Array, Elementliste)
+        Call Acquire_Costs(TRS_Array, TAL_Array, TRS_Akt, Elementliste)
 
         'Kosten aufsummieren
         Dim i, j As Integer
@@ -120,76 +130,8 @@ Public Class SKos
         System.Array.Resize(Array, x)
     End Sub
 
-    'Länge der Transportstrecken einlesen
-    '************************************
-    Private Sub Read_TRS_Laenge(ByVal BlueM1 As BlueM, ByRef TRS_Array(,) As Object)
-
-        'Dim TRS_Array(,) As Object = {}
-        Dim Datei As String = BlueM1.WorkDir & BlueM1.Datensatz & ".TRS"
-
-        Dim FiStr As FileStream = New FileStream(Datei, FileMode.Open, IO.FileAccess.ReadWrite)
-        Dim StrRead As StreamReader = New StreamReader(FiStr, System.Text.Encoding.GetEncoding("iso8859-1"))
-
-        Dim Zeile As String
-        Dim Anz As Integer = 0
-
-        'Anzahl der Zeilen feststellen
-        Do
-            Zeile = StrRead.ReadLine.ToString()
-            If (Zeile.StartsWith("*") = False) Then
-                Anz += 1
-            End If
-        Loop Until StrRead.Peek() = -1
-
-        Dim i As Integer = -1
-        Dim j As Integer = 0
-        ReDim TRS_Array(Anz - 1, 1)
-
-        'Zurück zum Dateianfang und lesen
-        FiStr.Seek(0, SeekOrigin.Begin)
-
-        Dim array() As String
-        Do
-            Zeile = StrRead.ReadLine.ToString()
-            If (Zeile.StartsWith("*") = False) Then
-                array = Zeile.Split("|")
-                'Werte zuweisen
-                TRS_Array(j, 0) = array(1).Trim()
-                TRS_Array(j, 1) = array(3).Trim()
-                j += 1
-            End If
-
-        Loop Until StrRead.Peek() = -1
-
-        StrRead.Close()
-        FiStr.Close()
-
-        'Array bereinigen
-        Dim x, y As Integer
-        Dim TmpArray(TRS_Array.GetUpperBound(0), 1) As String
-        System.Array.Copy(TRS_Array, TmpArray, TRS_Array.Length)
-        x = 0
-
-        For y = 0 To TmpArray.GetUpperBound(0)
-            If Not TmpArray(y, 0) = "" Then
-                If String.Compare(TmpArray(y, 0), 1, "S", 1, 1, True) Then
-                    TmpArray(x, 0) = TmpArray(y, 0)
-                    TmpArray(x, 1) = TmpArray(y, 1)
-                    x += 1
-                End If
-            End If
-        Next
-
-        ReDim TRS_Array(x - 1, 1)
-        For y = 0 To x - 1
-            TRS_Array(y, 0) = TmpArray(y, 0)
-            TRS_Array(y, 1) = TmpArray(y, 1)
-        Next
-
-    End Sub
-
-    'Flächen der Original Transportstrecken einlesen
-    '***********************************************
+    'Inforationen der Original Transportstrecken einlesen
+    '****************************************************
     Public Sub Read_TRS_Orig_Daten(ByVal BlueM1 As BlueM)
 
         Call Read_TRS_Daten(BlueM1, TRS_Orig)
@@ -197,9 +139,12 @@ Public Class SKos
     End Sub
 
 
-    'Flächen der Transportstrecken einlesen
-    '**************************************
+    'Inforationen der Transportstrecken einlesen
+    'Hier werden nur die Informationen einer Seite eingelesenund Symerie angenommen (könnte mann leicht erweitern)
+    '*************************************************************************************************************
     Public Shared Sub Read_TRS_Daten(ByVal BlueM1 As BlueM, ByRef TRS_Array() As TRS)
+
+        ReDim TRS_Array(-1)
 
         'Dim TRS_Array(,) As Object = {}
         Dim Datei As String = BlueM1.WorkDir & BlueM1.Datensatz & ".TRS"
@@ -220,56 +165,130 @@ Public Class SKos
 
         Dim i As Integer = -1
         Dim j As Integer = -1
-        Dim count as Integer = 0
+        Dim count As Integer = 0
 
         'Zurück zum Dateianfang und lesen
         FiStr.Seek(0, SeekOrigin.Begin)
 
         Dim Array1() As String
-        Dim Array2() As String
+        Dim Array2(6) As String
 
         Do
             Zeile = StrRead.ReadLine.ToString()
             If (Zeile.StartsWith("*") = False) Then
                 Array1 = Zeile.Split("|")
-                Array2 = Split(Array1(6))
-
-                'Filtert das Array auf leere Einträge
-                Dim LastNonEmpty As Integer = -1
-                For t As Integer = 0 To Array2.Length - 1
-                    If Array2(t) <> "" Then
-                        LastNonEmpty += 1
-                        Array2(LastNonEmpty) = Array2(t)
-                    End If
-                Next
-                ReDim Preserve Array2(LastNonEmpty)
+                Array2(0) = Array1(6).Substring(1, 1).Trim
+                Array2(1) = Array1(6).Substring(3, 6).Trim
+                Array2(2) = Array1(6).Substring(9, 6).Trim
+                Array2(3) = Array1(6).Substring(15, 5).Trim
+                Array2(4) = Array1(6).Substring(21, 6).Trim
+                Array2(5) = Array1(6).Substring(27, 5).Trim
+                Array2(6) = Array1(6).Substring(33, 5).Trim
 
                 'Werte zuweisen
                 'Prüfen ob neues Element
-                If Array1(1).Trim() <> "" And Array1(2).trim = "3" then
+                If Array1(1).Trim() <> "" And Array1(2).Trim = "3" Then
                     j += 1
                     count = 0
                     ReDim Preserve TRS_Array(TRS_Array.GetLength(0))
                     TRS_Array(j).Name = Array1(1).Trim()
                     TRS_Array(j).Laenge = Array1(3).Trim()
-                    TRS_Array(j).A_Breite_Sohle = array2(1).Trim()
+                    TRS_Array(j).A0_Hoehe_Sohle = Array2(1)
+                    TRS_Array(j).A1_Breite_Sohle_L = Array2(2)
+                    TRS_Array(j).A2_Breite_Sohle_R = Array2(4)
+
                 ElseIf Array1(1).Trim() = "" Then
                     count += 1
                 End If
 
-                If count = 1 then
-                    TRS_Array(j).B1_Höhe_Gerinne = Array2(0).Trim()
-                    TRS_Array(j).B2_Breite_Ufer = Array2(1).Trim()
-                else if count = 2
-                    TRS_Array(j).C1_Höhe_FPlain = Array2(1).Trim()
-                    TRS_Array(j).C2_Breite_Vorl = Array2(2).Trim()
-                else if count = 3
-                    TRS_Array(j).D1_Höhe_Rand = Array2(1).Trim()
-                    TRS_Array(j).D2_Breite_Rand = Array2(2).Trim()
+                If count = 1 Then
+                    TRS_Array(j).B0_Hoehe_Gerinne = Array2(1)
+                    TRS_Array(j).B1_Breite_Ufer_L = Array2(2)
+                    TRS_Array(j).B2_Breite_Ufer_R = Array2(4)
+                ElseIf count = 2 Then
+                    TRS_Array(j).C0_Hoehe_FPlain = Array2(1)
+                    TRS_Array(j).C1_Breite_Vorl_L = Array2(2)
+                    TRS_Array(j).C2_Breite_Vorl_R = Array2(4)
+                ElseIf count = 3 Then
+                    TRS_Array(j).D0_Hoehe_Rand = Array2(1)
+                    TRS_Array(j).D1_Breite_Rand_L = Array2(2)
+                    TRS_Array(j).D1_Breite_Rand_L = Array2(4)
                     count = 0
                 End If
             End If
         Loop Until StrRead.Peek() = -1
+
+        StrRead.Close()
+        FiStr.Close()
+
+    End Sub
+
+    'Verändertes Volumen und Kosten der Transportstrecken berechnen
+    '**************************************************************
+    Private Sub Calc_Volume(ByVal TRS_Orig() As TRS, ByVal TRS_Act() As TRS)
+
+        Dim i  As Integer
+        Dim L1, L2, H1, H2, F1, F2, Fges_orig, Fges_Act, Delta_L, Delta_R, Delta_Ges As Double
+
+        For i = 0 To TRS_Orig.GetUpperBound(0)
+
+            'Linke Seite Original
+            '********************
+            L1 = TRS_Orig(i).C1_Breite_Vorl_L - TRS_Orig(i).B1_Breite_Ufer_L
+            L2 = TRS_Orig(i).D1_Breite_Rand_L - TRS_Orig(i).C1_Breite_Vorl_L
+            H1 = (TRS_Orig(i).B0_Hoehe_Gerinne + TRS_Orig(i).C0_Hoehe_FPlain) / 2
+            H2 = (TRS_Orig(i).C0_Hoehe_FPlain + TRS_Orig(i).D0_Hoehe_Rand) / 2
+
+            F1 = L1 * H1
+            F2 = L2 * H2
+
+            Fges_orig = F1 + F2
+
+            'Linke Seite Aktuell
+            '*******************
+            L1 = TRS_Act(i).C1_Breite_Vorl_L - TRS_Act(i).B1_Breite_Ufer_L
+            L2 = TRS_Act(i).D1_Breite_Rand_L - TRS_Act(i).C1_Breite_Vorl_L
+            H1 = (TRS_Act(i).B0_Hoehe_Gerinne + TRS_Act(i).C0_Hoehe_FPlain) / 2
+            H2 = (TRS_Act(i).C0_Hoehe_FPlain + TRS_Act(i).D0_Hoehe_Rand) / 2
+
+            F1 = L1 * H1
+            F2 = L2 * H2
+
+            Fges_Act = F1 + F2
+
+            Delta_L = Fges_orig - Fges_Act
+
+            'Rechte Seite Original
+            '*********************
+            L1 = TRS_Orig(i).C2_Breite_Vorl_R - TRS_Orig(i).B2_Breite_Ufer_R
+            L2 = TRS_Orig(i).D2_Breite_Rand_R - TRS_Orig(i).C2_Breite_Vorl_R
+            H1 = (TRS_Orig(i).B0_Hoehe_Gerinne + TRS_Orig(i).C0_Hoehe_FPlain) / 2
+            H2 = (TRS_Orig(i).C0_Hoehe_FPlain + TRS_Orig(i).D0_Hoehe_Rand) / 2
+
+            F1 = L1 * H1
+            F2 = L2 * H2
+
+            Fges_orig = F1 + F2
+
+            'Rechte Seite Aktuell
+            '********************
+            L1 = TRS_Act(i).C2_Breite_Vorl_R - TRS_Act(i).B2_Breite_Ufer_R
+            L2 = TRS_Act(i).D2_Breite_Rand_R - TRS_Act(i).C2_Breite_Vorl_R
+            H1 = (TRS_Act(i).B0_Hoehe_Gerinne + TRS_Act(i).C0_Hoehe_FPlain) / 2
+            H2 = (TRS_Act(i).C0_Hoehe_FPlain + TRS_Act(i).D0_Hoehe_Rand) / 2
+
+            F1 = L1 * H1
+            F2 = L2 * H2
+
+            Fges_Act = F1 + F2
+
+            Delta_R = Fges_orig - Fges_Act
+
+            Delta_Ges = Delta_L + Delta_R
+
+            TRS_Act(i).Volume = Delta_Ges * TRS_Act(i).Laenge
+
+        Next
 
     End Sub
 
@@ -349,7 +368,7 @@ Public Class SKos
 
     'Weist den Bauwerken die Kosten zu
     '*********************************
-    Private Sub Acquire_Costs(ByVal TRS_Array(,) As Object, ByVal TAL_Array(,) As Object, ByVal Bauwerksliste(,) As Object)
+    Private Sub Acquire_Costs(ByVal TRS_Array(,) As Object, ByVal TAL_Array(,) As Object, ByVal TRS_act As TRS(), ByVal Bauwerksliste(,) As Object)
 
         Dim i, j As Integer
         Dim gefunden As Boolean = False
@@ -367,9 +386,9 @@ Public Class SKos
                     End If
                 Next
             ElseIf Bauwerksliste(i, 0).Startswith("S") Then
-                For j = 0 To TRS_Array.GetUpperBound(0)
-                    If Bauwerksliste(i, 0) = TRS_Array(j, 0) Then
-                        Laenge = TRS_Array(j, 1)
+                For j = 0 To TRS_act.GetUpperBound(0)
+                    If Bauwerksliste(i, 0) = TRS_act(j).Name Then
+                        Laenge = TRS_act(j).Laenge
                         'Kalkulation: 100€/lfm
                         Bauwerksliste(i, 1) = Laenge * 200
                         gefunden = True
