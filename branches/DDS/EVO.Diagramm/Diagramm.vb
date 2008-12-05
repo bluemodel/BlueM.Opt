b2fc1c1acdceb07fa9278cc64271d7174b548560
froehlich <froehlich@719ab8b7-b586-4fd8-a5da-7ca64381027e>
2008-12-05 09:49:29 +0000

    branch für DDS Algo

---
+++ branches/DDS/EVO.Diagramm/Diagramm.vb
@@ -0,0 +1,305 @@
+'*******************************************************************************
+'*******************************************************************************
+'**** Klasse Diagramm                                                       ****
+'****                                                                       ****
+'**** Erweiterung der Klasse Steema.TeeChart.TChart                         ****
+'****                                                                       ****
+'**** Autoren: Felix Froehlich, Christoph Hübner                            ****
+'****                                                                       ****
+'**** Fachgebiet Ingenieurhydrologie und Wasserbewirtschaftung              ****
+'**** TU Darmstadt                                                          ****
+'*******************************************************************************
+'*******************************************************************************
+
+Imports System.Drawing
+
+''' <summary>
+''' Klasse stellt Diagrammfunktionalitäten zur Verfügung
+''' </summary>
+''' <remarks>Erweiterung der Klasse Steema.TeeChart.TChart</remarks>
+Public Class Diagramm
+    Inherits Steema.TeeChart.TChart
+
+#Region "Eigenschaften"
+
+    Public Structure Achse
+        Public Title As String
+        Public Automatic As Boolean
+        Public Minimum As Double
+        Public Maximum As Double
+        Public Increment As Double
+    End Structure
+
+#End Region
+
+#Region "Diagrammfunktionen"
+
+    'Diagramm zurücksetzen
+    '*********************
+    Public Sub Reset()
+        With Me
+            .Clear()
+            .Header.Text = "EVO"
+            .Chart.Axes.Bottom.Title.Caption = ""
+            .Chart.Axes.Left.Title.Caption = ""
+        End With
+    End Sub
+
+    'Diagramm bearbeiten
+    '*******************
+    Public Sub TChartEdit(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Me.DoubleClick
+        Try
+            Me.ShowEditor()
+        Catch ex As Exception
+            MsgBox("Fehler in TeeChart!" & EVO.Common.eol & ex.Message, MsgBoxStyle.Critical)
+        End Try
+    End Sub
+
+#End Region 'Diagrammfunktionen
+
+#Region "Serienverwaltung"
+
+    'Serien-Initialisierung (Punkt)
+    'gibt die Serie zurück
+    '******************************
+    Public Function getSeriesPoint(ByVal title As String, _
+                                      Optional ByVal colorName As String = "", _
+                                      Optional ByVal style As Steema.TeeChart.Styles.PointerStyles = Steema.TeeChart.Styles.PointerStyles.Circle, _
+                                      Optional ByVal size As Integer = 3, _
+                                      Optional ByVal ColEach As Boolean = False) As Steema.TeeChart.Styles.Points
+
+        Dim i As Integer
+        Dim serie As Steema.TeeChart.Styles.Points
+
+        'Überprüfen, ob Serie bereits existiert
+        For i = 0 To Me.Chart.Series.Count - 1
+            If (Me.Chart.Series(i).Title = title) Then
+                serie = Me.Chart.Series(i)
+                Return serie
+            End If
+        Next
+
+        'Sonst Serie neu hinzufügen
+        serie = New Steema.TeeChart.Styles.Points(Me.Chart)
+        serie.Title = title
+        serie.Pointer.Style = style
+        serie.Pointer.HorizSize = size
+        serie.Pointer.VertSize = size
+        serie.ColorEach = ColEach
+        If (Not colorName = "") Then
+            serie.Color = Drawing.Color.FromName(colorName)
+        End If
+
+        Call Me.add_MarksTips(serie)
+        serie.Cursor = Windows.Forms.Cursors.Hand
+
+        Return serie
+
+    End Function
+
+    'Serien-Initialisierung (Linie)
+    'gibt die Serie zurück
+    '******************************
+    Public Function getSeriesLine(ByVal title As String, _
+                                      Optional ByVal colorName As String = "") As Steema.TeeChart.Styles.Line
+
+        Dim i As Integer
+        Dim serie As Steema.TeeChart.Styles.Line
+
+        'Überprüfen, ob Serie bereits existiert
+        For i = 0 To Me.Chart.Series.Count - 1
+            If (Me.Chart.Series(i).Title = title) Then
+                serie = Me.Chart.Series(i)
+                Return serie
+            End If
+        Next
+
+        'Sonst Serie neu hinzufügen
+        serie = New Steema.TeeChart.Styles.Line(Me.Chart)
+        serie.Title = title
+        If (Not colorName = "") Then
+            serie.Color = Drawing.Color.FromName(colorName)
+        End If
+
+        Call Me.add_MarksTips(serie, Steema.TeeChart.Styles.MarksStyles.XY)
+
+        Return serie
+
+    End Function
+
+    'Serien-Initialisierung (3DPunkt)
+    'gibt die Serie zurück
+    '********************************
+    Public Function getSeries3DPoint(ByVal title As String, _
+                                      Optional ByVal colorName As String = "", _
+                                      Optional ByVal style As Steema.TeeChart.Styles.PointerStyles = Steema.TeeChart.Styles.PointerStyles.Circle, _
+                                      Optional ByVal size As Integer = 3, _
+                                      Optional ByVal ColEach As Boolean = False) As Steema.TeeChart.Styles.Points3D
+
+        Dim i As Integer
+        Dim serie As New Steema.TeeChart.Styles.Points3D
+
+        'Überprüfen, ob Serie bereits existiert
+        For i = 0 To Me.Chart.Series.Count - 1
+            If (Me.Chart.Series(i).Title = title) Then
+                serie = Me.Chart.Series(i)
+                Return serie
+            End If
+        Next
+
+        'Sonst Serie neu hinzufügen
+        serie = New Steema.TeeChart.Styles.Points3D(Me.Chart)
+        serie.Title = title
+        serie.Pointer.Style = style
+        serie.Pointer.HorizSize = size
+        serie.Pointer.VertSize = size
+        serie.Pointer.Draw3D = True
+        serie.Depth = size
+        serie.LinePen.Visible = False
+        serie.ColorEach = ColEach
+        If (Not colorName = "") Then
+            serie.Color = Drawing.Color.FromName(colorName)
+        End If
+
+        'BUG 234: MarksTip funktioniert momentan nur in der XY-Ebene korrekt
+        Call Me.add_MarksTips(serie)
+        serie.Cursor = Windows.Forms.Cursors.Hand
+
+        Return serie
+
+    End Function
+
+    'Serien werden von Hinten gelöscht
+    '*********************************
+    Sub DeleteSeries(ByVal Max As Integer, ByVal Min As Integer)
+
+        Dim i As Integer
+        For i = Max To Min Step -1
+            If Me.Chart.Series.Count - 1 = i Then
+                Me.Chart.Series.Remove(Me.Chart.Series(i))
+            End If
+        Next
+
+    End Sub
+
+    'MarksTips zu einer Serie hinzufügen
+    '***********************************
+    Public Sub add_MarksTips(ByVal serie As Steema.TeeChart.Styles.Series, Optional ByVal style As Steema.TeeChart.Styles.MarksStyles = Steema.TeeChart.Styles.MarksStyles.Label)
+
+        Dim myMarksTip As Steema.TeeChart.Tools.MarksTip
+        myMarksTip = New Steema.TeeChart.Tools.MarksTip(Me.Chart)
+        myMarksTip.Series = serie
+        myMarksTip.MouseAction = Steema.TeeChart.Tools.MarksTipMouseAction.Move
+        myMarksTip.MouseDelay = 10 'millisekunden
+        myMarksTip.Style = style
+
+    End Sub
+
+#End Region 'Serienverwaltung
+
+#Region "Misc"
+
+    ''' <summary>
+    ''' Speichert die verwendeten Farben für die bisherigen Pfade und generiert neue, falls erforderlich
+    ''' </summary>
+    ''' <param name="ColorArray">Array von Farben (Objects)???</param>
+    ''' <param name="ind">Das CES-Individuum, für welches eine Farbe bestimmt werden soll</param>
+    ''' <returns>ein Farbe</returns>
+    ''' <remarks>nur für CES!</remarks>
+    Public Shared Function ColorManagement(ByRef ColorArray(,) As Object, ByVal ind As Common.Individuum_CES) As Color
+
+        'TODO: Funktion ColorManagement generisch machen!
+
+        Dim i, j As Integer
+        Dim count As Integer
+        Dim Farbe As Color = Color.White
+
+        'Falls der Pfad schon vorhanden ist wird diese Farbe verwendet
+        For i = 0 To ColorArray.GetUpperBound(1)
+            count = 0
+            For j = 1 To ColorArray.GetUpperBound(0)
+                If ColorArray(j, i) = ind.Path(j - 1) Then
+                    count += 1
+                End If
+            Next
+            If count = ind.Path.GetLength(0) Then
+                Farbe = ColorArray(0, i)
+            End If
+        Next
+
+
+        'Für Farbverläufe __________________________________________________________________________________
+
+        'If ColorAray.GetLength(1) = 0 then
+        '    ReDim Preserve ColorAray(ColorAray.GetUpperBound(0), ColorAray.GetLength(1))
+        '    Farbe = Color.FromArgb(255, 0, 255, 255)
+        'ElseIf Farbe = Color.White Then
+
+        '    Farbe = ColorAray(0, ColorAray.GetUpperBound(1))
+        '    ReDim Preserve ColorAray(ColorAray.GetUpperBound(0), ColorAray.GetLength(1))
+
+        '    Dim R As Integer = Farbe.R
+        '    Dim G As Integer = Farbe.G
+        '    Dim B As Integer = Farbe.B
+
+        '    G = G - 50
+        '    If G < 0 then
+        '        G = 255
+        '        B = B - 50
+        '        If B < 100
+        '            B = 255
+        '            R = R + 50
+        '            If R > 255
+        '                Throw New Exception("Die Anzahl der farben für die verschiedenen Pfade ist erschöpft")
+        '            End If
+        '        End If
+        '    End If
+
+        '    Farbe = color.FromArgb(255, R, G, B)
+        '    ColorAray(0, ColorAray.GetUpperBound(1)) = Farbe
+
+        '    For i = 1 To ColorAray.GetUpperBound(0)
+        '        ColorAray(i, ColorAray.GetUpperBound(1)) = ind.Path(i - 1)
+        '    Next
+
+        'End If
+
+
+        'Für zufällige Farben _________________________________________________________________________________
+
+        If Farbe = Color.White Then
+            ReDim Preserve ColorArray(ColorArray.GetUpperBound(0), ColorArray.GetLength(1))
+            Dim NeueFarbe As Boolean = True
+            Dim CountFarbe As Integer = 0
+            Do
+                Randomize()
+                'Genriert Zahl zwischen
+                Farbe = Drawing.Color.FromArgb(255, CInt(Int((50 * Rnd()) + 1)) * 5, _
+                                                    CInt(Int((50 * Rnd()) + 1)) * 5, _
+                                                    CInt(Int((50 * Rnd()) + 1)) * 5)
+                For i = 0 To ColorArray.GetUpperBound(1)
+                    If Farbe = ColorArray(0, i) Then
+                        NeueFarbe = False
+                    End If
+                Next
+                CountFarbe += 1
+
+                If CountFarbe > 15000 Then
+                    Farbe = Color.White
+                    NeueFarbe = True
+                End If
+                'If CountFarbe > 15000 Then Throw New Exception("Die Anzahl der farben für die verschiedenen Pfade ist erschöpft")
+            Loop Until NeueFarbe = True
+            ColorArray(0, ColorArray.GetUpperBound(1)) = Farbe
+            For i = 1 To ColorArray.GetUpperBound(0)
+                ColorArray(i, ColorArray.GetUpperBound(1)) = ind.Path(i - 1)
+            Next
+        End If
+
+        Return Farbe
+
+    End Function
+
+#End Region 'Misc
+
+End Class
