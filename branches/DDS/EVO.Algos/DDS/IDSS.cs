ae56bb4b1f5dfa7d20021c0b0bb3fec1b25f9907
muschalla <muschalla@719ab8b7-b586-4fd8-a5da-7ca64381027e>
2008-12-08 16:01:50 +0000

    DDS algorithm

---
+++ branches/DDS/EVO.Algos/DDS/IDSS.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace modelEAU.DDS
+{
+    /// <summary>
+    /// Interface of the DSS-class
+    /// </summary>
+    public interface IDSS
+    {
+        void initialize(double r_val_in, int maxiter_in, int num_dec_in, double[] ini_par);
+        void initialize(double r_val_in, int maxiter_in, int num_dec_in);
+        double[] ini_solution_candidate();
+        void ini_Fbest(double fvalue);
+        void update_Fbest(double fvalue);
+        void update_search_historie(double fvalue, int index);
+        void track_ini();
+        double[] determine_DV(int i);
+    }
+}
