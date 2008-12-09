06f84fada9617a0b55abe077e84edb0626d1f54e
froehlich <froehlich@719ab8b7-b586-4fd8-a5da-7ca64381027e>
2008-12-09 12:25:32 +0000

    branch für DDS2

---
+++ branches/DDS2/EVO.Kern/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Allgemeine Informationen über eine Assembly werden über die folgenden 
+' Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+' die mit einer Assembly verknüpft sind.
+
+' Die Werte der Assemblyattribute überprüfen
+
+<Assembly: AssemblyTitle("EVO.Kern")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("IHWB - TU Darmstadt")> 
+<Assembly: AssemblyProduct("EVO.Kern")> 
+<Assembly: AssemblyCopyright("Copyright © IHWB 2008")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)> 
+
+'Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+<Assembly: Guid("3bf04ae9-f9ff-48de-a80c-952a40d78425")> 
+
+' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+'
+'      Hauptversion
+'      Nebenversion 
+'      Buildnummer
+'      Revision
+'
+' Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+' übernehmen, indem Sie "*" eingeben:
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
