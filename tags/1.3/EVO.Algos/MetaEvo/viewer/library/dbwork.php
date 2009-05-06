1ad9a23b0814eac3e5b3fc763099922cbdb6a49e
froehlich <froehlich@719ab8b7-b586-4fd8-a5da-7ca64381027e>
2009-05-06 08:11:22 +0000

    tag für v1.3

---
+++ tags/1.3/EVO.Algos/MetaEvo/viewer/library/dbwork.php
@@ -0,0 +1,68 @@
+<?php
+//@mysql_query("SET NAMES 'utf8'");   UTF8 damit in sql und html gleich?
+
+function dbupdate($table,$vergleich,$datenarray,$debug) {
+	$edit = "UPDATE ".$table." SET ";
+		foreach ($datenarray as $key => $eintrag) {
+			$edit .= $key." = '".$eintrag."', "; 
+		}
+	$edit = rtrim($edit,", ");	
+	$edit .= " WHERE ".$vergleich;
+	if($debug) {//echo "<br /><br />Das bekommt dbupdate: "; print_r($datenarray); echo"<br />";
+		echo "<br /><br />Das bekommt dbupdate: ".$edit;
+	}
+	else if(mysql_query($edit)or die("<b>Fehler bei der Datenbankanfrage: </b>".mysql_error())){return true;}
+	else return false;
+}
+
+function dbinsert($table,$datenarray,$debug) {
+	$edit = "INSERT INTO ".$table." ("; 
+	$values = ") VALUES (";
+	foreach ($datenarray as $key => $eintrag) {
+			$edit .= $key.", ";
+			$values .= "'".$eintrag."', "; 
+		}
+	$edit = rtrim($edit,", ");	
+	$values = rtrim($values,", ");	
+	$edit .= $values.")"; 
+	if($debug) {//echo "<br /><br />Das bekommt dbinsert: "; print_r($datenarray); echo"<br />";
+		echo "<br /><br />Das bekommt dbinsert: ".$edit;
+	}
+	else if(mysql_query($edit)or die("<b>Fehler bei der Datenbankanfrage: </b>".mysql_error())){return true;}
+	else return false;
+}
+
+function dbdelete($table,$vergleich,$debug) {
+	$delete= "DELETE FROM ".$table." WHERE ".$vergleich."  LIMIT 1";
+	if($debug) {//echo "<br /><br />Das bekommt dbdelete: "; print_r($datenarray); echo"<br />";
+		echo "<br /><br />Das bekommt dbdelete: ".$delete;
+	}
+	else if(mysql_query($delete)or die("<b>Fehler bei der Datenbankanfrage: </b>".mysql_error())){return true;}
+	else return false;
+}
+
+function dbgetselected($table, $vergleich, $dbkey, $debug) {
+	$ask= "SELECT * FROM ".$table." WHERE ".$vergleich;
+	$content['info']['eintrage'] = 0;
+	
+	$do = mysql_query($ask);
+	$index = 0;
+	while($row = mysql_fetch_object($do)){
+		foreach ($row as $key => $value) {
+			if ($dbkey == "") $content[$index][$key] = $value;	
+			else $content[$row->$dbkey][$key] = $value;	
+		}
+		$index++;
+		$content['info']['eintrage']++;
+	}
+	if ($debug) {
+		echo"<br /><br />Das bekommt dbgetselected: ".$ask;
+		echo"<br /><br />Das liefert dbgetselected: ";
+		print_r($content);
+		return false;
+	}
+	else {
+		return $content;
+	}
+}
+?>
---
+++ tags/1.3/EVO.Apps/SWMM/SWMMThread.vb
@@ -0,0 +1,99 @@
+﻿'Klasse beinhaltet alle Infomationen für einen Simulationslauf im Thread
+'***********************************************************************
+Imports IHWB.SWMM.DllAdapter
+
+Public Class SWMMThread
+
+    Private Thread_ID As Integer
+    Private Child_ID As Integer
+    Private WorkFolder As String
+    Private DS_Name As String
+    Private SWMM_dll As SWMM_EngineDotNetAccess
+    Private SimIsOK As Boolean
+    Private launchReady As Boolean
+
+    Public Sub New(ByVal _Thread_ID As Integer, ByVal _Child_ID As Integer, ByVal _WorkFolder As String, ByVal _DS_Name As String, ByRef _SWMM_dll As SWMM_EngineDotNetAccess)
+        Me.Thread_ID = _Thread_ID
+        Me.Child_ID = _Child_ID
+        Me.WorkFolder = _WorkFolder
+        Me.DS_Name = _DS_Name
+        Me.SWMM_dll = _SWMM_dll
+    End Sub
+
+    'Die Funktion startet die Simulation mit dem entsprechendem WorkingDir
+    '*********************************************************************
+    Public Sub launchSim()
+
+        Me.SimIsOK = False
+        Me.launchReady = False
+
+        'Priority
+        System.Threading.Thread.CurrentThread.Priority = Threading.ThreadPriority.BelowNormal
+
+        Try
+            'Datensatz übergeben und initialisieren
+            Call SWMM_dll.Initialize(Me.WorkFolder & Me.DS_Name)
+			Call SWMM_dll.Start(1)
+
+            'Simulationszeitraum
+            Dim elapsedTime As Double = 0.0
+
+            Do
+                Call SWMM_dll.PerformTimeStep(elapsedTime)
+            Loop While (elapsedTime > 0.0)
+
+            'Simulation abschliessen
+            Call SWMM_dll.Finish()
+
+            'Simulation erfolgreich
+            Me.SimIsOK = True
+
+        Catch ex As Exception
+
+            'Simulationsfehler aufgetreten
+            MsgBox(ex.Message, MsgBoxStyle.Exclamation, "SWMM")
+
+            ''Simulation abschliessen
+            'Call SWMM_dll.Finish()
+
+            'Simulation nicht erfolgreich
+            Me.SimIsOK = False
+
+        Finally
+
+            'Ressourcen deallokieren
+            Call SWMM_dll.Dispose()
+
+        End Try
+
+        'Me.SimIsOK = False
+        Me.launchReady = True
+
+    End Sub
+
+    Public Function Sim_Is_OK() As Boolean
+
+        Sim_Is_OK = Me.SimIsOK
+    End Function
+
+    Public Function launch_Ready() As Boolean
+
+        launch_Ready = Me.launchReady
+    End Function
+
+    Public Sub set_is_OK()
+
+        Me.SimIsOK = True
+    End Sub
+
+    Public Function get_Thread_ID() As Integer
+
+        get_Thread_ID = Me.Thread_ID
+    End Function
+
+    Public Function get_Child_ID() As Integer
+
+        get_Child_ID = Me.Child_ID
+    End Function
+
+End Class
